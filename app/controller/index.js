51c28b5073228956339fd39c43ea8ea24628f1ee
eslam3bed <iah-93@hotmail.com>
2017-09-18 16:51:01 +0300

    setting up the enviroment inializing the project structure issue #11

---
+++ app/controller/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router;
+
+// delete the following when adding actual routes
+  // example for requiring controllers
+    // const login = require('./login.js');
+    // const signUp=require('./signUp.js')
+
+  // example for routes
+    // router.get('/login', login);
+    // router.get('/signUp', signUp);
+
+module.exports = routers;
