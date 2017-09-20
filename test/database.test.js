a6d567aeca9748e9b9c943e758d33dd36b094dd0
kefelhelou <kelhelouwork@gmail.com>
2017-09-20 03:29:32 +0300

    finish building and testing database #4

---
+++ test/database.test.js
@@ -0,0 +1,57 @@
+const connection = require('../app/models/database_build/db_connection.js');
+const test = require('tape');
+
+test('Testing database connection', (t) => {
+  const sql = '';
+  connection.query(sql, (err, res) => {
+    if (err) {
+      t.notOk(!err, err);
+      t.end();
+    } else {
+      t.equal(res != null, true, 'Should not be empty');
+      t.end();
+    }
+  });
+});
+
+test('Testing queries on the database', (t) => {
+  const sql = 'SELECT * FROM users';
+  connection.query(sql, (err, res) => {
+    if (err) {
+      t.notOk(!err, err);
+      t.end();
+    } else {
+      t.equal(res.rows.length, 3, 'Should not be empty');
+      t.end();
+      connection.end();
+    }
+  });
+});
+
+test('Testing queries on the database', (t) => {
+  const sql = 'SELECT * FROM customers';
+  connection.query(sql, (err, res) => {
+    if (err) {
+      t.notOk(!err, err);
+      t.end();
+    } else {
+      t.equal(res.rows.length, 3, 'Should not be empty');
+      t.end();
+      connection.end();
+    }
+  });
+});
+
+test('Testing queries on the database', (t) => {
+  const sql = 'SELECT * FROM debts';
+  connection.query(sql, (err, res) => {
+    if (err) {
+      t.notOk(!err, err);
+      t.end();
+    } else {
+      t.equal(res.rows.length, 6, 'Should not be empty');
+      t.end();
+      connection.end();
+    }
+  });
+});
