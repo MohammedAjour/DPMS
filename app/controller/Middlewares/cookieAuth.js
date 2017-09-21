aa9207f4b64a43407931275bb5f845399ef6cda5
eslam3bed <iah-93@hotmail.com>
2017-09-21 11:32:34 +0300

    adding cookie_Middleware

--- app/controller/Middlewares/cookieAuth.js
+++ app/controller/Middlewares/cookieAuth.js
@@ -1,3 +1,19 @@
-module.exports = (req, res, next) => {
+const cookies = require('cookie');
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+  const {cookie} = req;
+  if (!cookie) {
+    req.url = '/login';
+    next();
+  } else {
+    const token = cookies.parse(req.cookie).token;
+    jwt.verify(token, 'theSaltForSMDM', (err, user) => {
+      if (err) next(err);
+      else {
+        req.user = {username: user};
+        next();
+      }
+    });
+  }
 };
