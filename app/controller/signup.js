afad13f77676a3720a89642c16f38765a3a6d616
eslam3bed <iah-93@hotmail.com>
2017-09-25 12:54:05 +0300

    the signUp handler and handlebar view

--- app/controller/signup.js
+++ app/controller/signup.js
@@ -1,35 +1,35 @@
 const { isUser, signUpNewUser } = require('../models/queries/db_queries.js');
 const {regFormValidator} = require('../helpers/validator.js');
-
+const cookieEncrypt = require('../helpers/cookieEncrypt.js');
+const bcrypt = require('bcrypt');
 exports.get = (req, res, next) => {
-  console.log('login form mmmmmmmm');
-  // res.send('login nnnnnnnn');
-  res.render('signUp');
+  res.render('signUp', {valid: true});
 };
 exports.post = (req, res, next) => {
   // validate the form
   // check if the user already exist
   // insert the new user
-  console.log('asdasdasdasd', req.user);
-  res.send(req.user);
-  const user = req.user = {
-    username: req.body.name,
-    password: req.body.pwd,
-    email: req.body.email,
-    address: req.body.address
-  };
-  if (!regFormValidator(user).valid) {
-    return res.render('sign_up.hbs', {error: 'there is an error '});
-  }
+  const {user} = req;
+
+  if (!regFormValidator(user).valid) return res.render('signUp', regFormValidator(user));
   isUser(user.email, (err, exist) => {
     if (err) return next(err);
-    if (exist) return res.redirect('/login');          // the user already exist ,redirect him home
-    user.password = bcrypt.hashSync(user.password, 'theSaltForSMDM');
-    signUpNewUser(user, (err, insertionState) => {
+    if (exist) return res.redirect('/login');
+            // the user already exist ,redirect him home
+    bcrypt.genSalt(10, function (err, salt) {
       if (err) return next(err);
-      if (insertionState) {
-
-      }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        signUpNewUser(user, (err, insertionState) => {
+          if (err) return next(err);
+          const user = {name: insertionState.username, email: insertionState.email};
+          cookieEncrypt(user, (err, token) => {
+            if (err) return next(err);
+            res.cookie('token', token, { httpOnly: true });
+            res.redirect('/');
+          });
+        });
+      });
     });
   });
 };
